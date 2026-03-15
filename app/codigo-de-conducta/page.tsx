c22f0e262ff4e2f1682a358398d5f816dd6cb6d9
Jesus Aguirre <infoaguirrejesus@proton.me>
2026-03-14 22:29:59 -0500

    fix: update CODE_OF_CONDUCT_README_URL and correct version number in package.json.
    Signed-off-by: Jesus Aguirre <infoaguirrejesus@proton.me>

--- app/codigo-de-conducta/page.tsx
+++ app/codigo-de-conducta/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
-const CODE_OF_CONDUCT_README_URL = 'https://github.com/jugpanama/jugpanama/blob/main/README.md'
+const CODE_OF_CONDUCT_README_URL = 'https://github.com/jugpanama/website/blob/main/CODE_OF_CONDUCT.md'
 
 export default function CodigoDeConductaPage() {
   redirect(CODE_OF_CONDUCT_README_URL)
